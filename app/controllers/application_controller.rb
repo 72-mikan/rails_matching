5cf49193c34c39ad189169face4f41bde2ccd749
shinji_Note <shinzi7280.18@gmail.com>
2022-03-28 18:28:22 +0900

    user_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
 end
